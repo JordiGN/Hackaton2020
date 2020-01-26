a0770f5dbf90058615097c60cdfeee967168775b
Nate Nichols <nicho997@umn.edu>
2020-01-25 23:27:21 -0600

    improved posts screen

--- js/posts.js
+++ js/posts.js
@@ -1,9 +1,18 @@
 
 
 function display_post(post){
-
-
-
+    var poster = post["poster"];
+    var date = post["date"];
+    var title = post["title"];
+    var id = post["id"];
+    
+    var html = '<a href="" id="'+id+'"><div class="post">'
+	+'<div class="posttitle">'+title+'</div>'
+	+'<div class="postauthor">'+poster+" - "+date+'</div>'
+	+'</div></a>';
+    console.log(html);
+    $('#posts').append(html);
+    return;
 }
 
 
@@ -16,7 +25,14 @@ $(document).ready(function() {
     display_post({
 	title:"this is a test post",
 	date:"11/11/11",
-	poster:"richard dickson"
+	id:"6969696969",
+	poster:"Dirty Dan"
+    });
+    display_post({
+	title:"this is a test post",
+	date:"10/10/20",
+	id:"1231233",
+	poster:"T. Alberto Barbosa"
     });
 
 
