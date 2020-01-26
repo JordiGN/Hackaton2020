01c30407f866d6f766113d8eaa065d7a57078498
Jordi Garcia <jordi@tokyoplay.com>
2020-01-26 06:44:57 -0600

    login/logout/register logic

--- flasknew/app/models.py
+++ flasknew/app/models.py
@@ -1,15 +1,27 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import login
 from datetime import datetime
 from app import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def set_password(self, password):
+       self.password_hash = generate_password_hash(password) 
+
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.username)  
+ 
 
 class Project(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -18,7 +30,12 @@ class Project(db.Model):
 	date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	time = db.Column(db.Integer)
 	state = db.Column(db.String(2))
-	user_Id= db.Column(db.Integer, db.ForeignKey('user.id'))
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 	def __repr__(self):
 		return '<Project {}'.format(self.title)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
