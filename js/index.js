5684d0b81606489a0722f3800cbb296ebc020cd2
Nate Nichols <nicho997@umn.edu>
2020-01-25 22:22:06 -0600

    Added posts.html and posts.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     console.log("ready");
+    var state = location.search.substring(1);
+    console.log("state:"+state);
     $(".state").click(function(){
 	var state = $(this).text();
 	console.log(state);
