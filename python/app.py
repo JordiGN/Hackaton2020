682047e380f6dc5c63d2c3d5dc62f06a8aae86f1
Jay Madan <jaymadan@ebs-er-10-145-213-137.wireless.umn.edu>
2020-01-25 23:15:05 -0600

    added csv and modifed app.py

--- python/app.py
+++ python/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
-app = Flask(__name__)
+import pandas as pd 
+import numpy as np
+import flask
 
-@app.route("/locations/<state>")
+mainTable = pd.read_csv("raw_data.csv")
+
+#print(mainTable)
+
+def sortedFrame(frame, state):
+    sortedDataframe = frame[frame['state'] == state]
+    sortedDataframe['date'] = pd.to_datetime(sortedDataframe['date'], errors='coerce')
+    sortedDataframe = sortedDataframe.sort_values('date', ascending = True)
+    return sortedDataframe
+
+app = flask.Flask(__name__)
+
+@app.route('/locations/<state>')
+
 def getPostsByState(state):
-    
-    # return list of posts
-    return {}
+    return sortedFrame(mainTable, state).to_json(orient = 'table')
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
