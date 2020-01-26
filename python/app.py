7dccb27fbdf5a7fa3279895a7cc723a7a465c03b
Jordi Garcia <jordi@tokyoplay.com>
2020-01-26 03:14:04 -0600

    creating a new flask project, in an inside folder, I'm reusing code, so far, I have redirect, templates and login window

--- python/app.py
+++ python/app.py
@@ -1,33 +1,33 @@
-import pandas as pd 
-import numpy as np
-import flask
+# import pandas as pd 
+# import numpy as np
+# import flask
 
-mainTable = pd.read_csv("raw_data.csv")
+# mainTable = pd.read_csv("raw_data.csv")
 
-#print(mainTable)
+# #print(mainTable)
 
-def sortedFrame(frame, state):
-    sortedDataframe = frame[frame['state'] == state]
-    sortedDataframe['date'] = pd.to_datetime(sortedDataframe['date'], errors='coerce')
-    sortedDataframe = sortedDataframe.sort_values('date', ascending = True)
-    return sortedDataframe
+# def sortedFrame(frame, state):
+#     sortedDataframe = frame[frame['state'] == state]
+#     sortedDataframe['date'] = pd.to_datetime(sortedDataframe['date'], errors='coerce')
+#     sortedDataframe = sortedDataframe.sort_values('date', ascending = True)
+#     return sortedDataframe
 
-app = flask.Flask(__name__)
+# app = flask.Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     return "Hello, World!"
 
-@app.route("/createuser", method=["POST"])
-def create_user():
-    return CreateUserService().create(request.get_json())
-@app.route('/locations/<state>')
+# @app.route("/createuser", method=["POST"])
+# def create_user():
+#     return CreateUserService().create(request.get_json())
+# @app.route('/locations/<state>')
 
-def getPostsByState(state):
-    return sortedFrame(mainTable, state).to_json(orient = 'table')
+# def getPostsByState(state):
+#     return sortedFrame(mainTable, state).to_json(orient = 'table')
 
 
-if __name__ == "__main__":
-	Schema()
-    app.run(debug = True)
+# if __name__ == "__main__":
+# 	Schema()
+#     app.run(debug = True)
