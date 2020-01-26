75d5d466e2731d427fe6c19127d7b93c358660f7
r4huln1 <60283873+r4huln1@users.noreply.github.com>
2020-01-25 22:52:11 -0600

    Added database and sort function

---
+++ python/datatableCSV2020.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import os 
+
+
+mainTable = pd.read_csv("raw_data.csv")
+
+#print(mainTable)
+
+def sortedFrame(frame, state):
+    sortedDataframe = frame[frame['state'] == state]
+    sortedDataframe['date'] = pd.to_datetime(sortedDataframe['date'], errors='coerce')
+    sortedDataframe = sortedDataframe.sort_values('date', ascending = True)
+    return sortedDataframe
+
+
+print(sortedFrame(mainTable, "MN"))
+
